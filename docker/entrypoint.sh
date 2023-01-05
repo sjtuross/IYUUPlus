37c6954b8d9f607f8d48d929a4d0e6b428097f8b
Ross Shen <sjtuross@msn.com>
2023-01-05 13:19:56 +0800

    remove cron update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,32 +2,8 @@
 
 cd /IYUU
 
-if [[ ! -d .git ]]; then
-    git clone https://github.com/sjtuross/IYUUPlus.git /tmp/IYUU
-    find /tmp/IYUU -mindepth 1 -maxdepth 1 | xargs -I {} cp -r {} /IYUU
-    rm -rf /tmp/IYUU
-else
-    git fetch --all
-    git reset --hard origin/master
-    git pull
-fi
-
 if [[ ! -s .env ]]; then
     cp -f .env.example .env
 fi
 
-if [[ -z "${CRON_UPDATE}" ]]; then
-    minute=$(($RANDOM % 60))
-    hour_start=$(($RANDOM % 6))
-    hour_interval=$(($RANDOM % 4 + 6))
-    CRON_UPDATE="${minute} ${hour_start}-23/${hour_interval} * * *"
-fi
-
-echo "设置cron..."
-echo "${CRON_UPDATE} cd /IYUU && git fetch --all && git reset --hard origin/master && git pull && php start.php restart -d" | crontab -
-
-echo "当前crontab如下："
-crontab -l
-
-php /IYUU/start.php start -d
-crond -f
+php /IYUU/start.php start
